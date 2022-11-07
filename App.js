0dc148b8872f4f028ca7736099aea536cba5d00c
Veronica Silva <78879969+veronica-silva@users.noreply.github.com>
2022-11-07 09:24:48 -0300

    espaçamento de views, relembrando regras de grid

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import React from "react";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <SafeAreaView style={estilos.container}>
+      <View style={estilos.viewLogo}>
+        <Text>Megafirmes</Text>
+      </View>
 
-const styles = StyleSheet.create({
+      <View style={estilos.viewBotoes}>
+        <Button title="Buscar Filmes" />
+        <Button title="Favoritos" />
+      </View>
+
+      <View style={estilos.viewRodape}>
+        <Button title="Privacidade" />
+        <Button title="Sobre o App" />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
+
+const estilos = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "yellow",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  viewLogo: {
+    flex: 3,
+    backgroundColor: "green",
+    textAlign: "center",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    width: "80%",
+  },
+  viewBotoes: {
+    flex: 2,
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    backgroundColor: "orange",
+    width: "80%",
+    alignItems: "flex-start",
+  },
+  viewRodape: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "flex-start",
+    backgroundColor: "red",
+    width: "80%",
   },
 });
