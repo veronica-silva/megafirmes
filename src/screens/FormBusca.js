eb195946c7f3eaf870f96188e7e3cdfe3264c2a0
Veronica Silva <78879969+veronica-silva@users.noreply.github.com>
2022-11-09 10:59:08 -0300

    resolvendo exercício com componente de formbusca com alert, inputform e icon

--- src/screens/FormBusca.js
+++ src/screens/FormBusca.js
@@ -1,14 +1,82 @@
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  Button,
+  View,
+  Alert,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { useState } from "react";
 
 const FormBusca = () => {
+  const [texto, setTexto] = useState("");
+
+  const [textoDigitado, setTextoDigitado] = useState("");
+
+  const inputTexto = (valor) => {
+    setTexto(valor);
+  };
+
+  const buttonAlert = () => {
+    if (!texto) {
+      Alert.alert("Ops!", "Você deve digitar o nome de um filme", [
+        { texto: "OK" },
+      ]);
+      return;
+    }
+    Alert.alert("Você procurou por: ", texto), [{ texto: "ok" }];
+  };
+
   return (
-    <SafeAreaView>
-      <Text>FormBusca</Text>
+    <SafeAreaView style={styles.area}>
+      <Text style={styles.text}>
+        Star Trek? O Poderoso Chefão? A trilogia do Senhor dos Anéis?
+      </Text>
+      <Text style={styles.text}>
+        Localize um filme que você viu ou gostaria de ver!
+      </Text>
+      <View style={styles.caixa}>
+        <Ionicons name="film" size={40} color="black" style={styles.icon} />
+        <TextInput
+          style={styles.input}
+          placeholder="Filme..."
+          onChangeText={inputTexto}
+        />
+      </View>
+      <View style={styles.caixaBotao}>
+        <Button title="buscar" onPress={buttonAlert} color="#5451a6" />
+      </View>
     </SafeAreaView>
   );
 };
 
 export default FormBusca;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  area: {
+    padding: 15,
+    marginVertical: 10,
+  },
+  text: {
+    padding: 2,
+    justifyContent: "center",
+    fontSize: 18,
+    marginVertical: 5,
+  },
+  caixa: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+    marginVertical: 10,
+    justifyContent: "space-between",
+  },
+
+  input: {
+    flex: 1,
+    height: 35,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
