f922f97822208809c2060648bad287323496c1b6
Veronica Silva <veronica.lima.silva@uni9.edu.br>
2022-11-16 17:18:58 -0300

    itemVazio
    component

---
+++ src/components/ItemVazio.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const ItemVazio = () => {
+  return (
+    <View>
+      <Text>Não foram localizados filmes!</Text>
+    </View>
+  );
+};
+
+export default ItemVazio;
+
+const styles = StyleSheet.create({});
